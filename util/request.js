d5b8b1c979a4e6d3c63e6e4c9b7440345b953245
jol888 <50256997+jol888@users.noreply.github.com>
2026-08-09 00:23:49 +0800

    Add anonymous_token file creation and logging
    Check for the existence of 'anonymous_token' file and create it if it doesn't exist. Log the status of the file creation and update the token during startup.

--- util/request.js
+++ util/request.js
@@ -8,6 +8,24 @@ const tunnel = require('tunnel')
 const fs = require('fs')
 const path = require('path')
 const tmpPath = require('os').tmpdir()
+
+async function start() {
+  // 检测是否存在 anonymous_token 文件,没有则生成
+  if (!fs.existsSync(path.resolve(tmpPath, 'anonymous_token'))) {
+    fs.writeFileSync(path.resolve(tmpPath, 'anonymous_token'), '', 'utf-8')
+  }
+
+  console.log(fs.existsSync(path.resolve(tmpPath, 'anonymous_token'))? "[LOG] Created tmp file":"[ERROR] Werried")
+  
+  // 启动时更新anonymous_token
+  const generateConfig = require('./generateConfig')
+  await generateConfig()
+  require('./server').serveNcmApi({
+    checkVersion: true,
+  })
+}
+start()
+
 const anonymous_token = fs.readFileSync(
   path.resolve(tmpPath, './anonymous_token'),
   'utf-8',
